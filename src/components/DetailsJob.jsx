1536f3baa66072dd23ed3b5033c86bdd6913410d
Shohidul Arifin <110107262+arifin1987@users.noreply.github.com>
2024-11-27 10:29:24 -0800

    job Details page added

--- src/components/DetailsJob.jsx
+++ src/components/DetailsJob.jsx
@@ -3,11 +3,21 @@ import { useLoaderData, useParams } from "react-router-dom";
 const DetailsJob = () => {
   const details = useLoaderData();
   const { jobId } = useParams();
+  console.log(jobId);
   const item = details.find((detail) => detail.id == jobId);
 
   return (
     <div>
       <h1>{item.company_name}</h1>
+      <div className="grid md:grid-cols-4 gap-4">
+        <div className="border md:col-span-3">
+          <h2 className="text-2xl">Details coming soon</h2>
+        </div>
+        <div className="border">
+          <h2 className="text-2xl">Side things</h2>
+          <button className="btn btn-primary w-full">Apply Now</button>
+        </div>
+      </div>
     </div>
   );
 };
